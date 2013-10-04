466f0bbe91a031751c26768d3ed0d5380b551b7b
mpostelnicu <mpostelnicu@developmentgateway.org>
2013-10-05 00:12:30 +0300

    jetty + wicket + spring integration + spring security + spring-jpa + derby embedded working fine as multi-tier maven multimodule project!!!!

--- eudevfin/dim/src/main/java/org/devgateway/eudevfin/dim/ItemsPage.java
+++ eudevfin/dim/src/main/java/org/devgateway/eudevfin/dim/ItemsPage.java
@@ -5,19 +5,25 @@ import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.PropertyListView;
-import org.devgateway.eudevfin.domain.Item;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.devgateway.eudevfin.domain.Person;
+import org.devgateway.eudevfin.persistence.service.PersonService;
 
 @AuthorizeInstantiation("ROLE_SUPERVISOR")
 public class ItemsPage extends WebPage {
+	
+	@SpringBean
+	private PersonService personService;
 
 	public ItemsPage() {
 
-		add(new PropertyListView<Item>("items") {
+		
+		add(new PropertyListView<Person>("items",personService.findPeople()) {
 
 			@Override
-			protected void populateItem(ListItem<Item> item) {
-				item.add(new Label("product"));
-				item.add(new Label("quantity"));
+			protected void populateItem(ListItem<Person> item) {
+				item.add(new Label("name"));
+				item.add(new Label("id"));
 			}
 		});
 	}
--- eudevfin/dim/src/main/java/org/devgateway/eudevfin/dim/spring/WicketSpringApplication.java
+++ eudevfin/dim/src/main/java/org/devgateway/eudevfin/dim/spring/WicketSpringApplication.java
@@ -1,21 +1,37 @@
 package org.devgateway.eudevfin.dim.spring;
 
- import org.apache.wicket.authroles.authentication.AbstractAuthenticatedWebSession;
+ import java.util.Calendar;
+
+import org.apache.wicket.authroles.authentication.AbstractAuthenticatedWebSession;
 import org.apache.wicket.authroles.authentication.AuthenticatedWebApplication;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 import org.devgateway.eudevfin.dim.ItemsPage;
 import org.devgateway.eudevfin.dim.LoginPage;
+import org.devgateway.eudevfin.domain.Person;
+import org.devgateway.eudevfin.persistence.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class WicketSpringApplication extends AuthenticatedWebApplication {
 
-
+	@Autowired
+	private PersonService personService;
+	
 	@Override
 	protected void init() {
 		super.init();
 
 		getComponentInstantiationListeners().add(
 				new SpringComponentInjector(this));
+		
+		//test populating one person bean upon startup
+		final Person person = new Person();
+		Calendar createdDateTime = Calendar.getInstance();
+		createdDateTime.set(1980, 0, 1);
+		person.setCreatedDateTime(createdDateTime.getTime());
+		person.setName("bubu");
+		
+		personService.createPerson(person);
 	}
 
 	@Override
