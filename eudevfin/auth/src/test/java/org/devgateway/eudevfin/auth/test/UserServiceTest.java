b8a7787d89fd609c4a9c07c8e458ebc714ee0649
mpostelnicu <mpostelnicu@developmentgateway.org>
2013-10-25 09:23:32 +0300

    moved the tests to the auth module from common auth

--- eudevfin/auth/src/test/java/org/devgateway/eudevfin/auth/test/UserServiceTest.java
+++ eudevfin/auth/src/test/java/org/devgateway/eudevfin/auth/test/UserServiceTest.java
@@ -5,7 +5,6 @@ package org.devgateway.eudevfin.auth.test;
 
 import org.devgateway.eudevfin.auth.common.domain.User;
 import org.devgateway.eudevfin.auth.common.service.UserService;
-import org.devgateway.eudevfin.auth.common.test.AbstractAuthTest;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
