a77968e8d715e7b9ea114f84e04f090e753822dd
Fernando Ferreyra <fferreyra@developmentgateway.org>
2013-11-12 21:56:57 -0300

    changed user/pass by default back to admin/admin

--- eudevfin/common-auth/src/main/resources/derby-auth-startup.sql
+++ eudevfin/common-auth/src/main/resources/derby-auth-startup.sql
@@ -1,9 +1,9 @@
 delete from authorities;
 delete from users;
 
-insert into users (username, password, enabled) values ('elfleco', 'elfleco', true);
-insert into authorities (username, authority) values ('elfleco','ROLE_SUPERVISOR');
-insert into authorities (username, authority) values ('elfleco','ROLE_USER');
+insert into users (username, password, enabled) values ('admin', 'admin', true);
+insert into authorities (username, authority) values ('admin','ROLE_SUPERVISOR');
+insert into authorities (username, authority) values ('admin','ROLE_USER');
 
 INSERT INTO users (username, password, enabled) VALUES ('user', 'user', true);
 insert into authorities (username, authority) values ('user','ROLE_USER');
