11f4f94f76f94faa72518628c20f2a49e4734a34
Fernando Ferreyra <fferreyra@developmentgateway.org>
2013-12-01 23:52:57 -0300

    Added new cube and queries

--- eudevfin/financial/src/main/java/org/devgateway/eudevfin/financial/liquibase/PopulateMockupDb.java
+++ eudevfin/financial/src/main/java/org/devgateway/eudevfin/financial/liquibase/PopulateMockupDb.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
 
 import org.devgateway.eudevfin.financial.Category;
 import org.devgateway.eudevfin.financial.FinancialTransaction;
@@ -11,6 +12,7 @@ import org.devgateway.eudevfin.financial.Organization;
 import org.devgateway.eudevfin.financial.dao.CategoryDaoImpl;
 import org.devgateway.eudevfin.financial.dao.FinancialTransactionDaoImpl;
 import org.devgateway.eudevfin.financial.dao.OrganizationDaoImpl;
+import org.joda.money.BigMoney;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,7 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Component  
 public class PopulateMockupDb {
 	
-	public static int NUM_OF_TX	= 10;
+	public static int NUM_OF_TX	= 50;
 	
 	
 	@Autowired
@@ -36,18 +38,25 @@ public class PopulateMockupDb {
 		createOrganizations();
 		createSectors();
 		createTypesOfAid();
-		
+		Random r = new Random();
 		for (int i=1; i<=NUM_OF_TX; i++ ) {
 			FinancialTransaction tx 	= new FinancialTransaction();
-			tx.setAmount(new BigDecimal(1000));
+			tx.setCommitments(BigMoney.parse("EUR " + Math.ceil(Math.random()*100000)));
 			Organization org = null;
-			List<Organization> list = orgDao.findAllAsList();
-			if ( i<NUM_OF_TX/2 )
-				org = list.get(0);
-			else
-				org = list.get(1);
+			Category sector = null;
+			List<Organization> listOrgs = orgDao.findAllAsList();
+			List<Category> listSec = catDao.findAllAsList();
+			int randomIndex1=r.nextInt(5);
+			int randomIndex2=r.nextInt(5);
+			
+			System.out.println(randomIndex1);
+			System.out.println(randomIndex2);
+			
+			org = listOrgs.get(randomIndex1);
+			sector = listSec.get(randomIndex2);
 			tx.setReportingOrganization( org );
 			tx.setDescription("CDA Test Transaction " + i);
+			tx.setSector(sector);
 			txDao.save(tx);
 		}
 	}
@@ -66,6 +75,21 @@ public class PopulateMockupDb {
 		s2.setCode("510");
 		s2.setName("General Budget Support");
 		catDao.save(s2);
+
+		Category s3 = new Category();
+		s3.setCode("210");
+		s3.setName("Transport and Storage");
+		catDao.save(s3);
+
+		Category s4 = new Category();
+		s4.setCode("230");
+		s4.setName("Energy Generation and Support");
+		catDao.save(s4);
+
+		Category s5 = new Category();
+		s5.setCode("311");
+		s5.setName("Agriculture");
+		catDao.save(s5);
 
 		Category sectorScheme = new Category();
 		sectorScheme.setCode("000");
@@ -91,6 +115,24 @@ public class PopulateMockupDb {
 		o2.setName("CDA Test Org 2");
 		
 		orgDao.save(o2);
+
+		Organization o3 = new Organization();
+		o3.setCode("anOrgCode3");
+		o3.setName("CDA Test Org 3");
+		
+		orgDao.save(o3);
+
+		Organization o4 = new Organization();
+		o4.setCode("anOrgCode4");
+		o4.setName("CDA Test Org 4");
+		
+		orgDao.save(o4);
+
+		Organization o5 = new Organization();
+		o5.setCode("anOrgCode5");
+		o5.setName("CDA Test Org 5");
+		
+		orgDao.save(o5);
 	}
 
 	
