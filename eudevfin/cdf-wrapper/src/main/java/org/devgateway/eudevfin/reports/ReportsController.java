3215c09c81a795cbadc8309cf4bcdcef6aa4dd96
Ionut Dobre <idobre@developmentgateway.org>
2014-01-13 15:12:50 +0200

    Clean up ReportsController class.

--- eudevfin/cdf-wrapper/src/main/java/org/devgateway/eudevfin/reports/ReportsController.java
+++ eudevfin/cdf-wrapper/src/main/java/org/devgateway/eudevfin/reports/ReportsController.java
@@ -6,6 +6,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -23,8 +24,6 @@ import javax.sql.DataSource;
 
 import mondrian.olap.Connection;
 import mondrian.olap.DriverManager;
-import mondrian.olap.Query;
-import mondrian.olap.Result;
 import mondrian.olap.Util.PropertyList;
 import mondrian.rolap.RolapConnectionProperties;
 import net.sf.jasperreports.engine.JRException;
@@ -39,6 +38,7 @@ import net.sf.jasperreports.olap.JRMondrianQueryExecuterFactory;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -61,90 +61,6 @@ public class ReportsController {
 	private static final String OUTPUT_TYPE_HTML = "html";
 	private static final String OUTPUT_TYPE_CSV = "csv";
 	private static final String REPORT_YEAR = "reportYear";
-	
-	@RequestMapping(value = "/mondrian", method = RequestMethod.GET)
-    public ModelAndView generateMondrianReport(HttpServletRequest request, HttpServletResponse response, ModelAndView modelAndView)  throws IOException {
-		logger.info(">>> generate raport with Mondrian");
-				
-		PropertyList propertyList = new PropertyList();
-		propertyList.put("Provider", "mondrian");
-		propertyList.put("Catalog",
-				this.getClass().getResource("/org/devgateway/eudevfin/cda/service/financial.mondrian.xml").toString());
-		propertyList.put(RolapConnectionProperties.DynamicSchemaProcessor.toString(),
-	            "org.devgateway.eudevfin.cda.utils.SchemaProcessor");
-		
-		Connection conn = DriverManager.getConnection(propertyList, null, cdaDataSource);
-		
-		// used to test the connection
-		Query query = conn.parseQuery(
-			    "SELECT NON EMPTY {Hierarchize({[Measures].[Commitments Amount]})} ON COLUMNS," +
-			    " NON EMPTY CrossJoin({[BiMultilateral].[Bilateral en]}, [Type of Aid].[Name].Members) ON ROWS " +
-			    "FROM [Financial]");
-		@SuppressWarnings("deprecation")
-		Result result = conn.execute(query);
-		logger.info(result.getAxes().length);
-		
-		// parameters is used for passing extra parameters 
-		Map<String, Object> parameters = new HashMap<String, Object>();		
-		
-		InputStream inputStream;
-		try {
-			inputStream = ReportsController.class.getResourceAsStream("./jasper_template_mondrian.jrxml");
-			// retrieve the report template
-			JasperDesign jasperDesign = JRXmlLoader.load(inputStream);
-			// compile the report layout
-			JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
-			
-			// retrieve and compile the sub-report
-			JasperReport subreport1 = JasperCompileManager.compileReport(
-					JRXmlLoader.load(
-							ReportsController.class.getResourceAsStream("./report1_subreport3.jrxml")));
-
-			parameters.put(JRMondrianQueryExecuterFactory.PARAMETER_MONDRIAN_CONNECTION, conn);
-			parameters.put("Subreport1", subreport1);
-			// pass query parameters
-			parameters.put("DIM_NAME", "Name");
-			
-			// set locale
-			Locale locale = new Locale("ro");
-			parameters.put(JRParameter.REPORT_LOCALE, locale);
-			
-			// set resource bundle
-			try {
-				URL[] urls = {this.getClass().getResource("./").toURI().toURL()};
-				ClassLoader loader = new URLClassLoader(urls);
-				ResourceBundle resourceBundle = java.util.ResourceBundle.getBundle("i18n", locale, loader); 
-				parameters.put(JRParameter.REPORT_RESOURCE_BUNDLE, resourceBundle);
-			} catch (URISyntaxException e) {
-				e.printStackTrace();
-			}  
-			
-			// creates the JasperPrint object, it needs a JasperReport layout and extra params
-			JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters);
-			
-			// this is the stream where the data will be written
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			
-			// export XLS to output stream
-			ReportExporter reportExporter = new ReportExporter();
-			reportExporter.exportXLS(jasperPrint, baos);
-			
-			// set the response properties
-			String fileName = "Report.xls";
-			response.setHeader("Content-Disposition", "inline; filename=" + fileName);
-			
-			// make sure to set the correct content type
-			response.setContentType("application/vnd.ms-excel");
-			response.setContentLength(baos.size());
-			 
-			// write to response stream
-			this.writeReportToResponseStream(response, baos);
-		} catch (JRException e) {
-			e.printStackTrace();
-		}
-		
-        return null;
-    }
 	
 	/**
 	 * Generate a report
@@ -279,10 +195,28 @@ public class ReportsController {
 	private void generateDAC1 (HttpServletRequest request, HttpServletResponse response, 
 			Connection connection, String outputType) {
 		try {
-			InputStream inputStream = ReportsController.class.getResourceAsStream("./dac1_master.jrxml");
+			InputStream inputStream = ReportsController.class.getResourceAsStream("./dac1/dac1_master.jrxml");
 			
 			Map<String, Object> parameters = new HashMap<String, Object>();
 			parameters.put(JRMondrianQueryExecuterFactory.PARAMETER_MONDRIAN_CONNECTION, connection);
+			
+			// set locale
+			Locale locale = LocaleContextHolder.getLocale();			
+			parameters.put(JRParameter.REPORT_LOCALE, locale);
+			
+			// set resource bundle
+			try {
+				URL[] urls = {
+						this.getClass().getResource("./").toURI().toURL()
+				};
+				ClassLoader loader = new URLClassLoader(urls);
+				ResourceBundle resourceBundle = java.util.ResourceBundle.getBundle("i18n", locale, loader); 
+				parameters.put(JRParameter.REPORT_RESOURCE_BUNDLE, resourceBundle);
+			} catch (URISyntaxException e) {
+				e.printStackTrace();
+			} catch (MalformedURLException e) {
+				e.printStackTrace();
+			}
 			
 			JasperDesign jasperDesign = JRXmlLoader.load(inputStream);
 			JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
