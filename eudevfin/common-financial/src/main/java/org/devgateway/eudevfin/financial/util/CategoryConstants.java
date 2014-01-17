f3c13f618ccf7659338f0d01bc923662c84c877f
agartner@developmentgateway.org <agartner@developmentgateway.org>
2014-01-17 14:35:32 +0200

    Added constants for new categories

--- eudevfin/common-financial/src/main/java/org/devgateway/eudevfin/financial/util/CategoryConstants.java
+++ eudevfin/common-financial/src/main/java/org/devgateway/eudevfin/financial/util/CategoryConstants.java
@@ -26,6 +26,12 @@ public class CategoryConstants {
 	public static final String NATURE_OF_SUBMISSION_TAG		= "NATURE_OF_SUBMISSION_TAG";
 	public static final String INCOME_GROUP_TAG				= "INCOME_GROUP_TAG";
 	
+	public static final String TYPE_OF_REPAYMENT_TAG				= "TYPE_OF_REPAYMENT_TAG";
+	public static final String NUM_OF_REPAYMENTS_PER_ANNUM_TAG		= "NUM_OF_REPAYMENTS_PER_ANNUM_TAG";
+	public static final String SIGNIFICANCE_TAG						= "SIGNIFICANCE_TAG";
+	public static final String SIGNIFICANCE_FOR_PROGRAMME_TAG		= "SIGNIFICANCE_FOR_PROGRAMME_TAG";
+	
+	
 	/**
 	 * codes for ROOT categories
 	 */
@@ -37,6 +43,10 @@ public class CategoryConstants {
 	public static final String ROOT_NATURE_OF_SUBMISSION_CATEGORY		= "ROOT_NATURE_OF_SUBMISSION_CATEGORY";
 	public static final String ROOT_SECTOR_CATEGORY						= "ROOT_SECTOR_CATEGORY";
 	public static final String ROOT_CHANNEL_CATEGORY					= "ROOT_CHANNEL_CATEGORY";
+	public static final String ROOT_TYPE_OF_REPAYMENT_CATEGORY			= "ROOT_TYPE_OF_REPAYMENT_CATEGORY";
+	public static final String ROOT_NUM_OF_REPAYMENTS_PER_ANNUM			= "ROOT_NUM_OF_REPAYMENTS_PER_ANNUM";
+	public static final String ROOT_SIGNIFICANCE						= "ROOT_SIGNIFICANCE";
+	public static final String ROOT_SIGNIFICANCE_FOR_PROGRAMME			= "ROOT_SIGNIFICANCE_FOR_PROGRAMME";
 	
 			
 			
