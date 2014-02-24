375999fb5210cb481f80570a6bc850e0305d7baf
mpostelnicu <mpostelnicu@developmentgateway.org>
2014-02-24 14:45:34 +0200

    ODAEU-17 unit tests

--- eudevfin/auth/src/test/java/org/devgateway/eudevfin/auth/test/AbstractAuthTest.java
+++ eudevfin/auth/src/test/java/org/devgateway/eudevfin/auth/test/AbstractAuthTest.java
@@ -16,7 +16,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:META-INF/authContext.xml",
 		"classpath:META-INF/commonAuthContext.xml",
-		"classpath:META-INF/financialContext.xml" })
+		"classpath:META-INF/financialContext.xml",
+		"classpath:META-INF/importMetadataContext.xml",		
+		"classpath:META-INF/commonContext.xml"})
 public abstract class AbstractAuthTest {
 	
 	protected static Logger logger = Logger.getLogger(AbstractAuthTest.class);
