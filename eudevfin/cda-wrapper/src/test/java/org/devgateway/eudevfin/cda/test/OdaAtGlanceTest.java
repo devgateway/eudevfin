8aa854aeda215149dbb9438dfd40f34aaacabaf2
Ionut Dobre <idobre@developmentgateway.org>
2014-02-25 17:32:07 +0200

    Added new Unit Tests for CDA. (ODAEU-127)

--- eudevfin/cda-wrapper/src/test/java/org/devgateway/eudevfin/cda/test/OdaAtGlanceTest.java
+++ eudevfin/cda-wrapper/src/test/java/org/devgateway/eudevfin/cda/test/OdaAtGlanceTest.java
@@ -19,6 +19,7 @@ import org.joda.time.LocalDateTime;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -142,7 +143,7 @@ public class OdaAtGlanceTest {
 
         // add 2nd transaction
         listTypeOfFlow = catDao.findByCode("TYPE_OF_FLOW##10");
-        listSectors = catDao.findByCode("321");
+        listSectors = catDao.findByCode("322");
         listTypeOfFinance = catDao.findByCode("TYPE_OF_FINANCE##110");
         listTypeOfAid = catDao.findByCode("A");
         listBiMultilateral = catDao.findByCode("BI_MULTILATERAL##1");
@@ -154,7 +155,7 @@ public class OdaAtGlanceTest {
         typeOfFinance = listTypeOfFinance.get(0);
         typeOfAid = listTypeOfAid.get(0);
         biMultilateral = listBiMultilateral.get(0);
-        area = findAreaByName(listAreas, "Romania");
+        area = findAreaByName(listAreas, "Tunisia");
 
         ft = new CustomFinancialTransaction();
 
@@ -180,7 +181,7 @@ public class OdaAtGlanceTest {
 
         // add 3rd transaction
         listTypeOfFlow = catDao.findByCode("TYPE_OF_FLOW##10");
-        listSectors = catDao.findByCode("321");
+        listSectors = catDao.findByCode("112");
         listTypeOfFinance = catDao.findByCode("TYPE_OF_FINANCE##110");
         listTypeOfAid = catDao.findByCode("A");
         listBiMultilateral = catDao.findByCode("BI_MULTILATERAL##2");
@@ -192,7 +193,7 @@ public class OdaAtGlanceTest {
         typeOfFinance = listTypeOfFinance.get(0);
         typeOfAid = listTypeOfAid.get(0);
         biMultilateral = listBiMultilateral.get(0);
-        area = findAreaByName(listAreas, "Romania");
+        area = findAreaByName(listAreas, "Turkey");
 
         ft = new CustomFinancialTransaction();
 
@@ -209,6 +210,42 @@ public class OdaAtGlanceTest {
         ft.setFormType("bilateralOda.advanceQuestionnaire");
         ft.setBiMultilateral(biMultilateral);
         ft.setRecipient(area);
+
+        ft.setAmountsExtended(BigMoney.parse("EUR " + 1000000));
+        ft.setExtendingAgency(organization);
+
+        txDao.save(ft);
+        transactions.add(ft);           // save the transaction so we can delete it later
+
+        // add 4th transaction that is not ODA
+        listTypeOfFlow = catDao.findByCode("TYPE_OF_FLOW##30");
+        listSectors = catDao.findByCode("321");
+        listTypeOfFinance = catDao.findByCode("TYPE_OF_FINANCE##110");
+        listTypeOfAid = catDao.findByCode("A");
+        listBiMultilateral = catDao.findByCode("BI_MULTILATERAL##2");
+        channel = channelCatDao.findByCode("41301").getEntity();        // add a chanel with a coefficient (it should be 51% in this case)
+
+        sector = listSectors.get(0);
+        typeOfFlow = listTypeOfFlow.get(0);
+        typeOfFinance = listTypeOfFinance.get(0);
+        typeOfAid = listTypeOfAid.get(0);
+        biMultilateral = listBiMultilateral.get(0);
+
+        ft = new CustomFinancialTransaction();
+
+        ft.setDraft(false);
+        ft.setChannel(channel);
+        ft.setLocale("en");
+        ft.setShortDescription( "Auditing test description 4");
+        ft.setDescription("Long - " + ft.getDescription() );
+        ft.setSector(sector);
+        ft.setTypeOfFlow(typeOfFlow);
+        ft.setTypeOfFinance(typeOfFinance);
+        ft.setTypeOfAid(typeOfAid);
+        ft.setReportingYear(LocalDateTime.parse((2013) + "-07-01"));
+        ft.setFormType("bilateralOda.advanceQuestionnaire");
+        ft.setBiMultilateral(biMultilateral);
+        ft.setRecipient(null);
 
         ft.setAmountsExtended(BigMoney.parse("EUR " + 1000000));
         ft.setExtendingAgency(organization);
@@ -301,21 +338,15 @@ public class OdaAtGlanceTest {
         Assert.assertNotNull(txDao);
         Assert.assertNotNull(orgDao);
 
-        Assert.assertEquals("Check number of transactions", 7, txDao.findAllAsList().size());
+        Assert.assertEquals("Check number of transactions", 8, txDao.findAllAsList().size());
     }
 
     @Test
     public void testNetODATable () throws Exception {
-        logger.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
-
-//        logger.info(txDao.findAllAsList());
-        logger.info("txDao size: " + txDao.findAllAsList().size());
-
         Map<String, String> params = new HashMap<>();
         params.put("dataAccessId", "netODATable");
 
         QueryResult result = CdaService.doQuery(params);
-        logger.info("getResultset: " + result.getResultset().toString());
 
         /*
         * From PopulateExchangeDbChange.java file we have the following exchange rates:
@@ -326,37 +357,89 @@ public class OdaAtGlanceTest {
         Assert.assertEquals("Check Net ODA Table query response",
                 "[[Curent (USD), 128000, 1993200], [National Currency, 100000, 1510000], [ODA/GNI, null, 30], [Bilateral share, 100, 50]]",
                 result.getResultset().toString());
-
-
-        logger.info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
     }
 
-//    @Test
-//    public void testTopTenRecipientsTable () {
-//
-//    }
-//
-//    @Test
-//    public void testTopTenMemoShareTAble () {
-//
-//    }
-//
-//    @Test
-//    public void testOdaByRegionChart () {
-//
-//    }
-//
-//    @Test
-//    public void testOdaByIncomeGroupChart () {
-//
-//    }
-//
-//    @Test
-//    public void testOdaBySectorChart () {
-//
-//    }
+    @Test
+    public void testTopTenRecipientsTable () {
+        Map<String, String> params = new HashMap<>();
+        params.put("dataAccessId", "topTenRecipients");
 
-    // test ODA/GNI/Population (non flow)
+        QueryResult result = CdaService.doQuery(params);
+
+        // there are 3 transactions with 3 different recipient countries: Tunisia, Turkey, Moldova
+        Assert.assertEquals("Check Top Ten Recipients query number of rows", "3", result.getQueryInfo().getTotalRows());
+        Assert.assertEquals("Check Top Ten Recipients query response",
+                "[[Tunisia, 1320000], [Turkey, 673200], [Moldova, 128000]]",
+                result.getResultset().toString());
+    }
+
+    @Test
+    public void testTopTenMemoShareTable () {
+        Map<String, String> params = new HashMap<>();
+        params.put("dataAccessId", "topTenMemoShare");
+
+        QueryResult result = CdaService.doQuery(params);
+
+        Assert.assertEquals("Check Top Ten Memo Share query number of rows", "1", result.getQueryInfo().getTotalRows());
+        Assert.assertEquals("Check Top Ten Memo Share query response",
+                "[[100]]",
+                result.getResultset().toString());
+    }
+
+    @Ignore("Not ready yet. We need to find a relation between country and regions")
+    @Test
+    public void testOdaByRegionChart () {
+        Map<String, String> params = new HashMap<>();
+        params.put("dataAccessId", "odaByRegionChart");
+
+        QueryResult result = CdaService.doQuery(params);
+
+        Assert.assertEquals("Check Oda By Region query number of rows", "1", result.getQueryInfo().getTotalRows());
+        Assert.assertEquals("Check Oda By Region query response",
+                "[[something]]",
+                result.getResultset().toString());
+    }
+
+    @Test
+    public void testOdaByIncomeGroupChart () {
+        Map<String, String> params = new HashMap<>();
+        params.put("dataAccessId", "odaByIncomeGroupChart");
+
+        QueryResult result = CdaService.doQuery(params);
+
+        Assert.assertEquals("Check Oda By Income Group query number of rows", "2", result.getQueryInfo().getTotalRows());
+        // Tunisia and Turkey are in UMICs and Moldova in LMICs
+        Assert.assertEquals("Check Oda By Income Group query response",
+                "[[UMICs, 1993200], [LMICs, 128000]]",
+                result.getResultset().toString());
+    }
+
+    @Test
+    public void testOdaBySectorChart () {
+        Map<String, String> params = new HashMap<>();
+        params.put("dataAccessId", "odaBySectorChart");
+
+        QueryResult result = CdaService.doQuery(params);
+
+        // we used 3 different sectors
+        Assert.assertEquals("Check Oda By Sector query number of rows", "3", result.getQueryInfo().getTotalRows());
+        Assert.assertEquals("Check Oda By Sector query response",
+                "[[Basic education, ODA (Official Development Assistance), 673200], [INDUSTRY, ODA (Official Development Assistance), 128000], [MINERAL RESOURCES AND MINING, ODA (Official Development Assistance), 1320000]]",
+                result.getResultset().toString());
+    }
+
+    @Test
+    public void testNonFlowData () {
+        Map<String, String> params = new HashMap<>();
+        params.put("dataAccessId", "nonFlowData");
+
+        QueryResult result = CdaService.doQuery(params);
+
+        Assert.assertEquals("Check Non Flow Data query number of rows", "4", result.getQueryInfo().getTotalRows());
+        Assert.assertEquals("Check Non Flow Data query response",
+                "[[GNI: Gross National Income, 60], [ODA % GNI, 30], [Total flows % GNI, 46], [Population, 10000]]",
+                result.getResultset().toString());
+    }
 
     private Area findAreaByName(List<Area> listAreas, String name) {
         for(Area area : listAreas) {
