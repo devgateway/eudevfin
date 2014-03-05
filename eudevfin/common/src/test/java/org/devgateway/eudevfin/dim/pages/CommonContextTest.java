456182def28b3a79707324dce9cc5093b7760234
Alexandru Artimon <aartimon@developmentgateway.org>
2014-03-05 18:22:30 +0200

    ODAEU-167 -common - isolation test - common-ui - TextInputField required type test, render test                        - HeaderFooter - render test

---
+++ eudevfin/common/src/test/java/org/devgateway/eudevfin/dim/pages/CommonContextTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2014 Development Gateway.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ */
+
+package org.devgateway.eudevfin.dim.pages;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Alexandru Artimon
+ * @since 04/03/14
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+        "classpath:META-INF/commonContext.xml"
+})
+public class CommonContextTest {
+
+    @Before
+    public void setUp() throws Exception {
+        //application.setApplicationContext(context);
+    }
+
+    @Test
+    public void testContextIsolation() {
+        assertEquals(1L, 1L);
+    }
+
+}
