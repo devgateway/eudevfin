73f14063a2f3b9a87d1c2c8989c0b73fb607b05d
Alexandru Artimon <aartimon@developmentgateway.org>
2014-03-26 13:14:41 +0200

    ODAEU-167 (cherry picked from commit 6485d5e)

--- eudevfin/dim/src/test/java/org/devgateway/eudevfin/dim/core/BaseWicketTest.java
+++ eudevfin/dim/src/test/java/org/devgateway/eudevfin/dim/core/BaseWicketTest.java
@@ -1,38 +1,55 @@
-/*******************************************************************************
- * Copyright (c) 2013 Development Gateway.
+/*
+ * Copyright (c) 2014 Development Gateway.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the GNU Public License v3.0
  * which accompanies this distribution, and is available at
  * http://www.gnu.org/licenses/gpl.html
- *
- * Contributors:
- *    aartimon
- ******************************************************************************/
+ */
 
 package org.devgateway.eudevfin.dim.core;
 
 import org.apache.wicket.util.tester.WicketTester;
-import org.devgateway.eudevfin.ui.common.spring.WicketSpringApplication;
+import org.devgateway.eudevfin.ui.common.spring.TestingWicketSpringApplication;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
- * @author aartimon@developmentgateway.org
- * @since 23 October 2013
+ * @author Alexandru Artimon
+ * @since 05/03/14
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:META-INF/dimContext.xml", "classpath:META-INF/authContext.xml", "classpath:META-INF/commonAuthContext.xml", "classpath:/META-INF/financialContext.xml", "classpath:/META-INF/commonFinancialContext.xml"})
+@ContextConfiguration(locations = {
+        "classpath:META-INF/commonContext.xml",
+        "classpath:META-INF/commonAuthContext.xml",
+        "classpath:META-INF/commonFinancialContext.xml",
+        "classpath:META-INF/*Context.xml"
+})
 public abstract class BaseWicketTest {
     protected WicketTester tester;
 
     @Autowired
-    private WicketSpringApplication app;
+    private ApplicationContext ctx;
 
     @Before
-    public void setUp() throws Exception {
-        tester = new WicketTester(app);
+    public void setUp() {
+        TestingWicketSpringApplication application = new TestingWicketSpringApplication();
+        // applicationContext is injected by Spring as my test class extends AbstractJUnit4SpringContextTests
+        AutowireCapableBeanFactory autowireCapableBeanFactory = ctx.getAutowireCapableBeanFactory();
+        //autowireCapableBeanFactory.autowireBeanProperties(application, AutowireCapableBeanFactory.AUTOWIRE_AUTODETECT, false );
+        autowireCapableBeanFactory.autowireBean(application);
+
+        application.setApplicationContext(ctx);
+        tester = new WicketTester(application);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        tester.destroy();
     }
 }
