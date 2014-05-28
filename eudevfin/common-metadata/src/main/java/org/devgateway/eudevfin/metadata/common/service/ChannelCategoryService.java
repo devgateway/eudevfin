6404a4fee547ca9bca1c52e98878e0d98817c53b
agartner <agartner@developmentgateway.org>
2014-05-28 14:19:00 +0300

    ODAEU-209
    creating special functionality for channel category for searching also
    by acronym.
    Included test.

--- eudevfin/common-metadata/src/main/java/org/devgateway/eudevfin/metadata/common/service/ChannelCategoryService.java
+++ eudevfin/common-metadata/src/main/java/org/devgateway/eudevfin/metadata/common/service/ChannelCategoryService.java
@@ -6,6 +6,8 @@ package org.devgateway.eudevfin.metadata.common.service;
 import org.devgateway.eudevfin.common.service.BaseEntityService;
 import org.devgateway.eudevfin.common.spring.integration.NullableWrapper;
 import org.devgateway.eudevfin.metadata.common.domain.ChannelCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.integration.annotation.Header;
 
 /**
@@ -15,4 +17,10 @@ import org.springframework.integration.annotation.Header;
 public interface ChannelCategoryService extends BaseEntityService<ChannelCategory> {
 
 	public NullableWrapper<ChannelCategory> findByCode(String string, @Header("initializeChildren") Boolean initializeChildren);
+	
+	public Page<ChannelCategory> findByGeneralSearchAndTagsCodePaginated(
+			@Header("locale") String locale, String searchString,
+			@Header("tagsCode") String tagsCode,
+			@Header("pageable") Pageable page,
+			@Header("initializeChildren") Boolean initializeChildren);
 }
